e94bf1fccdf1d4f90098a2a6a28f00d2f76151c7
Filipp Nis <f.nis61@gmail.com>
2025-04-30 08:26:52 +0300

    Решение задачи 4

--- task_4.py
+++ task_4.py
@@ -1,37 +1,36 @@
 class EmployeeSalary:
     hourly_payment = 400  # Устанавливаем почасовой уровень оплаты
 
-    def __init__(self, name, hours=None, rest_days=0, email=None):
+    def __init__(self, name, hours, rest_days, email):
         self.name = name
-        self.hours = hours
-        self.rest_days = rest_days
-        self.email = email
+        self.hours = hours  # Обязательный параметр для инициализации
+        self.rest_days = rest_days  # Обязательный параметр для инициализации
+        self.email = email  # Обязательный параметр для инициализации
 
     @classmethod
-    def get_hours(cls, name, rest_days):
+    def get_hours(cls, name, rest_days, email):
         hours = (7 - rest_days) * 8  # Рассчитываем часы работы
-        return cls(name=name, hours=hours, rest_days=rest_days)
+        return cls(name, hours, rest_days, email)  # Создаем и возвращаем объект
 
     @classmethod
-    def get_email(cls, name):
+    def get_email(cls, name, hours, rest_days):
         email = f"{name}@email.com"  # Генерируем email
-        return cls(name=name, email=email)
+        return cls(name=name, hours=hours, rest_days=rest_days, email=email)  # Создаем и возвращаем объект
 
     @classmethod
     def set_hourly_payment(cls, new_payment):
         cls.hourly_payment = new_payment  # Меняем значение hourly_payment
 
     def salary(self):
-        if self.hours is None:  # Если часы не известны, рассчитываем их
-            self.hours = (7 - self.rest_days) * 8
         return self.hours * self.hourly_payment  # Рассчитываем заработную плату
 
+
 # Пример использования:
-employee1 = EmployeeSalary.get_hours("Иван", rest_days=2)
+email_ivan = "ivan@example.com"  # Здесь задайте нужный email
+employee1 = EmployeeSalary.get_hours("Иван", rest_days=2, email=email_ivan)
 print(f"Заработная плата {employee1.name}: {employee1.salary()}")
 
-# Генерация email для сотрудника
-employee1.email = EmployeeSalary.get_email(employee1.name).email
+# Вывод email для сотрудника
 print(f"Email {employee1.name}: {employee1.email}")
 
 # Изменение почасовой оплаты
